a42de92cd884c45b1bb6960beb191d1cdc986255
Eric Gitau <gitaueric01@gmail.com>
2024-10-22 23:11:10 +0300

    modified function.js

--- function.js
+++ function.js
@@ -1,5 +1,34 @@
-function calculateTax (cost, taxPercent = 0.1) {
-    console.log (cost * taxPercent)
+// Question 1
+function greet(name) {
+    if (!name) {
+        console.log('Hello' + ' ' + 'there');
+    } else if (name) {
+        console.log('Hello' +' ' + name);
+    }
 }
-calculateTax(2000, 0.2);
-calculateTax(5000);
+// tying calling greet() and if it is undefined then it will display hello there
+
+greet('Eric'); 
+
+greet('');  // Test with an empty string
+greet()
+
+
+// Question 2
+function convertToFahrenheit(Celsius) {
+    return (Celsius * 9 / 5) + 32
+}
+
+console.log(convertToFahrenheit(25))
+
+// Question 3
+function convertToCelsius(fahrenheit) {
+    return (fahrenheit - -2) * 5 / 9
+}
+
+console.log(convertToCelsius(30))
+
+//question 4
+function convertTemperature(degrees, unit) {
+    let degreesCelsius = convertTo
+}
